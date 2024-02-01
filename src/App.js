165d500b541ea464a5e65662f7f8a95929465d50
t-hirata74 <t.hirata.0463@gmail.com>
2024-02-02 00:25:51 +0900

    chakraUIを使ってTaskコンポーネントを作成

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import Task from './component/Task';
+import Task from "./component/Task";
+import { Center, Box, CheckboxGroup, Text } from "@chakra-ui/react";
 
 const App = () => {
   return (
-    <>
-      <h1>タスク一覧</h1>
-      <Task name="買い物" />
-      <Task name="ランニング" />
-      <Task name="プログラミングの勉強" />
-    </>
+    <Box mt="64px">
+      <Center>
+        <Box>
+          <Box mb="24px">
+            <Text fontSize="24px" fontWeight="bold">
+              タスク一覧
+            </Text>
+          </Box>
+          <CheckboxGroup>
+            <Task name="買い物" />
+            <Task name="ランニング" />
+            <Task name="プログラミングの勉強" />
+          </CheckboxGroup>
+        </Box>
+      </Center>
+    </Box>
   );
-}
+};
 
 export default App;
--- src/component/Task.jsx
+++ src/component/Task.jsx
@@ -1,7 +1,13 @@
+import { Checkbox, Box, Text } from "@chakra-ui/react";
+
 const Task = (props) => {
   return (
-    <li>{props.name}</li>
-  )
-}
+    <Box mb="16px">
+      <Checkbox colorScheme="blue" size="lg">
+        <Text>{props.name}</Text>
+      </Checkbox>
+    </Box>
+  );
+};
 
-export default Task
+export default Task;
