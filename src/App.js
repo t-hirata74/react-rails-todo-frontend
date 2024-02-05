b2526bb3e0bf750bb49e3a71eff844cf0e4cb3b7
t-hirata74 <t.hirata.0463@gmail.com>
2024-02-06 00:42:44 +0900

    react hooksでタスクの設定

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import React, { useState, useEffect } from "react";
 import Task from "./component/Task";
 import { Center, Box, CheckboxGroup, Text } from "@chakra-ui/react";
 
 const App = () => {
+  const initialTasks = [
+    {
+      name: "買い物",
+      isDone: true,
+    },
+    {
+      name: "ランニング",
+      isDone: false,
+    },
+    {
+      name: "プログラミングの勉強",
+      isDone: false,
+    },
+  ];
+
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(initialTasks);
+  }, []);
+
+  const toggleIsDone = (index) => {
+    const tasksCopy = [...tasks];
+    const isDone = tasksCopy[index].isDone;
+    tasksCopy[index].isDone = !isDone;
+    setTasks(tasksCopy);
+  };
+
   return (
     <Box mt="64px">
       <Center>
@@ -12,9 +41,17 @@ const App = () => {
             </Text>
           </Box>
           <CheckboxGroup>
-            <Task name="買い物" />
-            <Task name="ランニング" />
-            <Task name="プログラミングの勉強" />
+            {tasks.map((task, index) => {
+              return (
+                <Task
+                  key={index}
+                  index={index}
+                  name={task.name}
+                  isDone={task.isDone}
+                  toggleIsDone={toggleIsDone}
+                />
+              );
+            })}
           </CheckboxGroup>
         </Box>
       </Center>
--- src/component/Task.jsx
+++ src/component/Task.jsx
@@ -3,7 +3,14 @@ import { Checkbox, Box, Text } from "@chakra-ui/react";
 const Task = (props) => {
   return (
     <Box mb="16px">
-      <Checkbox colorScheme="blue" size="lg">
+      <Checkbox
+        isChecked={props.isDone}
+        colorScheme="blue"
+        size="lg"
+        onChange={() => {
+          props.toggleIsDone(props.index);
+        }}
+      >
         <Text>{props.name}</Text>
       </Checkbox>
     </Box>
