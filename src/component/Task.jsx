a7100c872616261a7941061edc6b63e1fe4bb752
t-hirata74 <t.hirata.0463@gmail.com>
2024-04-01 21:38:43 +0900

    CloseIconを追加

--- src/component/Task.jsx
+++ src/component/Task.jsx
@@ -1,8 +1,9 @@
-import { Checkbox, Box, Text } from "@chakra-ui/react";
+import { Checkbox, Flex, Text } from "@chakra-ui/react";
+import { CloseIcon } from "@chakra-ui/icons";
 
 const Task = (props) => {
   return (
-    <Box mb="16px">
+    <Flex mb="16px" justifyContent="space-between" alignItems="center">
       <Checkbox
         isChecked={props.isDone}
         colorScheme="blue"
@@ -13,7 +14,8 @@ const Task = (props) => {
       >
         <Text>{props.name}</Text>
       </Checkbox>
-    </Box>
+      <CloseIcon onClick={() => props.destroyTask(props.id)} />
+    </Flex>
   );
 };
 
